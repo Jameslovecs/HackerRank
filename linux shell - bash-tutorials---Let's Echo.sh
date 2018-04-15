37f45e2e0b9c886a4a7f157e02c0a1c7ea0b7f90
ranrancao <rcao004@ucr.edu>
2018-04-15 12:29:42 -0700

    Update linux shell - bash-tutorials---Let's Echo.sh

--- linux shell - bash-tutorials---Let's Echo.sh
+++ linux shell - bash-tutorials---Let's Echo.sh
@@ -2,12 +2,12 @@
 BASH programs.
 Bash Tutorials - Let's Echo
 echo "Greetings" 
-→ 输出字符串里的东西
+# 输出字符串里的东西
 echo "Greetings $USER, your current working directory is $PWD"
--->输出user与当前目录信息，如下：（记住是用$这个符号！）
+# 输出user与当前目录信息，如下：（记住是用$这个符号！）
 Greetings prashantb1984, your current working directory is /home/prashantb1984 
-
 Ranran Cao
 '
+
 #!/bin/bash
 echo "HELLO"

6c8c5df6b187f0f1cde025249c4bf864156381be
ranrancao <rcao004@ucr.edu>
2018-04-15 12:23:29 -0700

    Update linux shell - bash-tutorials---looping-and-skipping.sh

--- linux shell - bash-tutorials---looping-and-skipping.sh
+++ linux shell - bash-tutorials---looping-and-skipping.sh
@@ -1,10 +1,12 @@
-/*
+:'
 BASH programs.
 Bash Tutorials - Looping and Skipping
-
+ for loops in Bash can be used in several ways: 
+- iterating between two integers,  and  
+- iterating between two integers,  and , and incrementing by  each time 
+- iterating through the elements of an array, etc.
 Ranran Cao
-*/
-
+'
 #!/bin/bash
 for i in {1..99..2}; do
     echo $i
